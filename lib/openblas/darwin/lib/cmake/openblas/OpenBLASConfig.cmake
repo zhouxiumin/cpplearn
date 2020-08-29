395113f416aabf340a3f8cf0a5b2d2025e312a25
zhouxiumin <zhouxiuminmeiwen@gmail.com>
2020-08-29 23:51:34 +0800

    add openblas lib

---
+++ lib/openblas/darwin/lib/cmake/openblas/OpenBLASConfig.cmake
@@ -0,0 +1,3 @@
+SET(OpenBLAS_VERSION "0.3.10.dev")
+SET(OpenBLAS_INCLUDE_DIRS /Users/zhouxiumin/github/clionProjects/cpplearn/lib/openblas/darwin/include)
+SET(OpenBLAS_LIBRARIES /Users/zhouxiumin/github/clionProjects/cpplearn/lib/openblas/darwin/lib/libopenblas.dylib)
